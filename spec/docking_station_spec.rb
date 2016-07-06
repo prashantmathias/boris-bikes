efcfbf8aef6f437908a4ad31b6fc697b8bfe7315
Arunas Skirius <arukomp@gmail.com>
2016-07-06 15:05:36 +0100

    Step 10 - Making Docking Stations Get Bikes Completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,9 @@ describe DockingStation do
   context 'responds to' do
 
     it { is_expected.to respond_to :release_bike }
+    it 'gets a bike and expects it to be working' do
+      expect(subject.release_bike).to be_working
+    end
 
   end
 end
