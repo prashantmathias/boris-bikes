fd71a46217a68e5f2c7261d1d3cbc6e9cccd021a
prashantmathias <mathiasaudio@gmail.com>
2016-07-06 15:59:41 +0100

    step 11 using instance variables completed

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,5 +4,4 @@ describe Bike do
   it "should respond to 'working?'" do
     expect(subject).to respond_to :working?
   end
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,20 @@ describe DockingStation do
 
     it { is_expected.to respond_to :release_bike }
     it 'gets a bike and expects it to be working' do
-      expect(subject.release_bike).to be_working
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it "should respond to 'dock'" do
+      expect(subject).to respond_to(:dock).with(1).argument
+      bike = subject.release_bike
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it "should be able to see the bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
     end
 
   end
