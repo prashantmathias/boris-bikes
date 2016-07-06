81d5bd2e2cad0fe2f2c26b55ef14ba5167b16863
Arunas Skirius <arukomp@gmail.com>
2016-07-06 12:26:02 +0100

    Step 8 - Back to the Unit completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,14 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
   context 'responds to' do
-  it 'release_bike' do
-    #it { is_expected.to respond_to :release_bike }
-    expect(subject).to respond_to :release_bike
-  end
 
-  it 'working?' do
-    #it { is_expected.to respond_to :release_bike }
-    expect(subject).to respond_to :working?
+    it { is_expected.to respond_to :release_bike }
+
   end
-end
 end
